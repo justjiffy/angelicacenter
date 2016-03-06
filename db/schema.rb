11595910b8ca6767ff882af3cad49fafc1e628c7
Juli Emmel <justjiffy@gmail.com>
2016-03-06 15:04:21 -0800

    added crud for classes/course

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160216052430) do
+ActiveRecord::Schema.define(version: 20160306222919) do
 
   create_table "boards", force: :cascade do |t|
     t.string   "first_name"
